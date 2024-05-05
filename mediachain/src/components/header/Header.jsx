68c1ba035d246f95c117c880194431fa9b7b7d2c
ivolavacek <ivo.lavacek@gmail.com>
2024-05-05 17:53:40 -0300

    frontend homepage

--- mediachain/src/components/header/Header.jsx
+++ mediachain/src/components/header/Header.jsx
@@ -1,11 +1,20 @@
 import './Header.css'
+import logo from '../../images/logo1.png'
+import name from "../../images/project-name1.png"
 
 function Header() {
     return (
         <>
             <div className="header">
-                <button className="button">Cadastro</button>
-                <button className="button">Login</button>
+                <div className="logo-container">
+                    <img src={logo} alt="logo" class="logo" />
+                    <img src={name} alt="name" class="name" />
+                </div>
+
+                <div className="button-container">
+                    <button className="button">Cadastro</button>
+                    <button className="button">Login</button>
+                </div>
             </div>
         </>
     )
